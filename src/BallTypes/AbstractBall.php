ae1223c5511ca25893fb870eef24f639359e4d86
Marwan <4160@htl.rennweg.at>
2018-05-07 21:48:46 +0200

    mein erster commit in 4 jahren

---
+++ src/BallTypes/AbstractBall.php
@@ -0,0 +1,52 @@
+<?php
+namespace Baelle\BallTypes;
+
+    include "Baelle/Interfaces/BallIinterface.php";
+    use Baelle\Interfaces\BallInterface as BallInterface;
+
+    abstract class AbstractBall implements BallInterface {
+        public $name;
+        public $durchmesser;
+        public $material;
+        //protected $volumen;
+
+        public function getName(): string
+        {
+            return $this->name;
+        }
+
+        public function getDurchmesser(): float
+        {
+            return $this->durchmesser;
+        }
+
+        public function getMaterial(): string
+        {
+            return $this->material;
+        }
+
+        function __construct(string $name, float $durchmesser, string $material)
+        {
+            $this->name = $name;
+            $this->durchmesser = $durchmesser;
+            $this->material = $material;
+
+        }
+
+        public function __toString() : string
+        {
+            
+            $rv = <<<EOT
+        Name: $this->name<br />
+        Durchmesser: $this->durchmesser<br />
+        Material: $this->material<br />
+EOT;
+            return $rv;
+        }
+
+        public function getVolumen(): float
+        {
+            return (1/3)*4*($this->durchmesser/2)*($this->durchmesser/2)*($this->durchmesser/2)*pi();
+        }
+    }
+?>
---
+++ src/BallTypes/ball.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andlk
+ * Date: 06.05.2018
+ * Time: 14:04
+ */
+
+
+namespace Baelle\BallTypes;
+//interface eibinden
+use Baelle\Interfaces\BallInterface as BallInterface;
+
+require_once  "Baelle\BallTypes\AbstractBall.php";
+include  "Baelle\BallTypes\AbstractBall.php";
+
+
+class Ball extends AbstractBall implements BallInterface{
+    /**
+     * @return float
+     */
+    public function getVolumen(): float
+    {
+       return (1/3)*4*($this->durchmesser/2)*($this->durchmesser/2)*($this->durchmesser/2)*pi();
+    }
+
+    function __construct(string $name, float $durchmesser, string $material)
+    {
+        $this->name = $name;
+        $this->durchmesser = $durchmesser;
+        $this->material = $material;
+
+    }
+
+
+
+}
+
